d4582440ae2e0c7207f33fa27ae237107a5e5baa
Md Rokonuzzaman <rokonjust@gmail.com>
2024-12-07 17:30:21 +0600

    Add New Campaign Page Form Added

--- src/pages/AddNewCampaign.jsx
+++ src/pages/AddNewCampaign.jsx
@@ -3,9 +3,67 @@ import React from "react";
 const AddNewCampaign = () => {
   return (
     <div>
-      <h1 className="text-4xl font-bold text-center">
-        This is Add New Campaign Page
-      </h1>
+      <div className="card bg-base-100 w-full lg:max-w-screen-lg max-w-sm mx-auto shrink-0 shadow-2xl">
+        <form className="card-body">
+          {/* Photo and Title Row */}
+          <div className="form-control lg:flex-row justify-between items-center">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Campaign Title</span>
+              </label>
+              <input
+                type="text"
+                name="text"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Photo URL</span>
+              </label>
+              <input
+                type="text"
+                required
+                name="photo"
+                placeholder="Photo URL"
+                className="input input-bordered"
+              />
+            </div>
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+            />
+            <label className="label">
+              <a href="#" className="label-text-alt link link-hover">
+                Forgot password?
+              </a>
+            </label>
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary">Login</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
